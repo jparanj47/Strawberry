eb419cd894f0ddc2b65afccda71e3299f21893d4
jparanj47 <89950550+jparanj47@users.noreply.github.com>
2021-12-20 19:25:02 -0500

    y

--- covidFunctionJP.py
+++ covidFunctionJP.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
   
-names = []
-marks = []
+counties = []
+cases = []
   
 f = open('umdCounty.txt','r')
 for row in f:
     row = row.split(' ')
-    names.append(row[0])
-    marks.append(int(row[1]))
+    counties.append(row[0])
+    cases.append(float(int(row[0])))
   
-plt.bar(names, marks, color = 'g', label = 'File Data')
+plt.bar(counties, cases, color = 'g', label = 'File Data')
   
-plt.xlabel('Student Names', fontsize = 12)
-plt.ylabel('Marks', fontsize = 12)
+plt.xlabel('County', fontsize = 12)
+plt.ylabel('Case Number', fontsize = 12)
   
-plt.title('Students Marks', fontsize = 20)
+plt.title('Cases per County', fontsize = 20)
 plt.legend()
 plt.show()
